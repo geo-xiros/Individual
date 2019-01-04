f55e2f84d3edceb90fe7d2bbc0e6595e59bd4c7f
George Xiros <geo.xiros@gmail.com>
2019-01-04 07:22:16 +0200

    Fix messagebox chices using dictionary

--- Individual/Forms/MessageBox.cs
+++ Individual/Forms/MessageBox.cs
@@ -6,36 +6,41 @@ using System.Threading.Tasks;
 
 namespace Individual
 {
-  static class MessageBox
-  {
-    public enum MessageBoxResult { Yes, No };
-    static public MessageBoxResult Show(string message)
+    static class MessageBox
     {
-      Console.CursorVisible = false;
-      ShowMessageInBox(message);
+        private static Dictionary<ConsoleKey, MessageBoxResult> _listOfChoices;
+        public enum MessageBoxResult { Yes, No };
+        static MessageBox()
+        {
+            _listOfChoices = new Dictionary<ConsoleKey, MessageBoxResult>() {
+                {ConsoleKey.Y, MessageBoxResult.Yes },
+                {ConsoleKey.N, MessageBoxResult.No },
+                {ConsoleKey.Escape, MessageBoxResult.No },
+            };
+        }
+        static public MessageBoxResult Show(string message)
+        {
+            Console.CursorVisible = false;
+            ShowMessageInBox(message);
+            ConsoleKey k;
 
-      ConsoleKey k = ConsoleKey.NoName;
-      while ((k != ConsoleKey.Y)
-          && (k != ConsoleKey.N))
-      {
-        k = Console.ReadKey(true).Key;
-      }
+            do
+            {
+                k = Console.ReadKey(true).Key;
+            } while (!_listOfChoices.ContainsKey(k));
 
-      if (k == ConsoleKey.Y)
-        return MessageBoxResult.Yes;
-      else
-        return MessageBoxResult.No;
+            return _listOfChoices[k];
+        }
+
+        static private void ShowMessageInBox(string message)
+        {
+            int x = Console.WindowWidth / 2 - message.Length / 2;
+            int y = Console.WindowHeight / 3;
+
+            ColoredConsole.Write($"\x250C{new string('\x2500', message.Length + 2)}\x2510", x - 2, y - 1, ConsoleColor.White);
+            ColoredConsole.Write($"\x2502{new string(' ', message.Length + 2)}\x2502", x - 2, y, ConsoleColor.White);
+            ColoredConsole.Write($"\x2514{new string('\x2500', message.Length + 2)}\x2518", x - 2, y + 1, ConsoleColor.White);
+            ColoredConsole.Write($"{message}", x, y, ConsoleColor.Green);
+        }
     }
-
-    static private void ShowMessageInBox(string message)
-    {
-      int x = Console.WindowWidth / 2 - message.Length / 2;
-      int y = Console.WindowHeight / 3;
-
-      ColoredConsole.Write($"\x250C{new string('\x2500', message.Length + 2)}\x2510", x - 2, y - 1, ConsoleColor.White);
-      ColoredConsole.Write($"\x2502{new string(' ', message.Length + 2)}\x2502", x - 2, y, ConsoleColor.White);
-      ColoredConsole.Write($"\x2514{new string('\x2500', message.Length + 2)}\x2518", x - 2, y + 1, ConsoleColor.White);
-      ColoredConsole.Write($"{message}", x, y, ConsoleColor.Green);
-    }
-  }
 }
