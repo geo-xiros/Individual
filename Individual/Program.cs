a2c95d84d0124078b495c23ad9761eb5cc67ac63
george <geo.xiros@gmail.com>
2019-01-07 21:33:09 +0200

    insert database schema file

--- Individual/Program.cs
+++ Individual/Program.cs
@@ -2,8 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
-
+using System.Reflection;
 
 namespace Individual
 {
@@ -18,9 +19,21 @@ namespace Individual
             }
 
             Console.Clear();
-            //Console.WriteLine("Bye.");
-        }
 
+            //Console.WriteLine("Bye.");
+
+        }
+        //static string GetFromResources(string resourceName)
+        //{
+        //    var assembly = Assembly.GetExecutingAssembly();
+        //    var resourceName = "Individual.DatabaseSchema.txt";
+
+        //    using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+        //    using (StreamReader reader = new StreamReader(stream))
+        //    {
+        //        string result = reader.ReadToEnd();
+        //    }
+        //}
 
     }
 }
