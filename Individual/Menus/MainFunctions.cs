8c80356d98c9fe2da4ecc41bba6de0ae8ed38345
geo.xiros <geo.xiros@gmailcom>
2019-01-16 12:58:02 +0200

    Fix error handling getusers from database

--- Individual/Menus/MainFunctions.cs
+++ Individual/Menus/MainFunctions.cs
@@ -21,13 +21,9 @@ namespace Individual.Menus
         {
             _loggedUser.LoadMessagesMenu(_menuController);
         }
-        public  void OthersMessagesMenu()
+        public void OthersMessagesMenu()
         {
-            var users = Database.GetUsers()
-                .Where(u => u.UserId != _loggedUser.UserId)
-                .OrderBy(u => u.LastName)
-                .Select(u => new KeyValuePair<int, string>(u.UserId, u.ToString()))
-                .ToList();
+            var users = GetUsersWithoutLoggedUser();
 
             ListMenu lm = new ListMenu("Select User to view messages", users, string.Format("A/A\x2502{0,-50}\x2502{1,-50}", "Lastname", "Firstname"));
             lm.ChooseListItem();
@@ -35,14 +31,35 @@ namespace Individual.Menus
             if (lm.Id != 0)
             {
                 User messagesUser = Database.GetUserBy(lm.Id);
+                if (messagesUser == null)
+                {
+                    return;
+                }
                 _loggedUser.LoadOthersMessagesMenu(_menuController, messagesUser);
             }
         }
+        private List<KeyValuePair<int, string>> GetUsersWithoutLoggedUser()
+        {
+
+            var users = Database.GetUsers();
+
+            if ((users?.Count() ?? 0) == 0)
+            {
+                return new List<KeyValuePair<int, string>>();
+            }
+
+            return users
+                .Where(u => u.UserId != _loggedUser.UserId)
+                .OrderBy(u => u.LastName)
+                .Select(u => new KeyValuePair<int, string>(u.UserId, u.ToString()))
+                .ToList();
+        }
+
         public void AccountManagmentMenu()
         {
             AccountsFunctions accountsMenu = new AccountsFunctions(_loggedUser);
 
-            _menuController.LoadMenu("Accounts Managment",new Dictionary<ConsoleKey, MenuItem>() {
+            _menuController.LoadMenu("Accounts Managment", new Dictionary<ConsoleKey, MenuItem>() {
                 { ConsoleKey.D1, new MenuItem("1. Create", accountsMenu.CreateUser) },
                 { ConsoleKey.D2, new MenuItem("2. View/Edit/Delete", accountsMenu.ViewEditDeleteMenu) },
                 { ConsoleKey.Escape, new MenuItem("[Esc] => Back", _menuController.LoadPreviousMenu) }
